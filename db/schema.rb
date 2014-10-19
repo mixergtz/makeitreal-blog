a371056b4b98a6a1782e6bb41bb11321aaef5569
Mixer Gutierrez <mixergutierrez@gmail.com>
2014-10-19 13:41:35 -0500

    Add articles relationship to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019173919) do
+ActiveRecord::Schema.define(version: 20141019182804) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
