b5bf532ead85d6bde532cbb3b910048290124871
Mixer Gutierrez <mixergutierrez@gmail.com>
2014-10-21 10:44:05 -0500

    Fixed reviews

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,23 +4,21 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
 
   def index
-	  @articles = Article.all
-	  @last_articles = Article.all.order('created_at DESC')
-    # @most_commented = Article.comments.count
+    @articles = Article.all
+    @last_articles = Article.all.order('created_at DESC')
   end
 
   def new
-  	@article = Article.new
+    @article = Article.new
   end
 
   def create
-	  @article = Article.new(article_params)
-
-  	if	@article.save
-  	  redirect_to @article
-  	else
+    @article = Article.new(article_params)
+    if	@article.save
+      redirect_to @article
+    else
       render "new"
-	  end
+    end
   end
 
   def show
@@ -33,16 +31,16 @@ class ArticlesController < ApplicationController
 
   def update
 
-	  if @article.update(article_params)
- 	    redirect_to @article
-	  else
-	    render "edit"
-	  end
+    if  @article.update(article_params)
+      redirect_to @article
+    else
+      render "edit"
+    end
   end
 
   def destroy
-	  @article.destroy
-	  redirect_to articles_path
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
@@ -52,7 +50,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-	  params.require(:article).permit(:title,:text)
+    params.require(:article).permit(:title,:text)
   end
 
 end
