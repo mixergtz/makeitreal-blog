0505201e3532988c6498149df892e6ea2310d048
Mixer Gutierrez <mixergutierrez@gmail.com>
2014-10-22 13:18:48 -0500

    Add limit to most commented scope

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
 
-  scope :most_commented, -> { joins(:comments).group("articles.id").select("articles.*, count(*) count_all").order("count_all desc") }
+  scope :most_commented, -> { joins(:comments).group("articles.id").select("articles.*, count(*) count_all").order("count_all desc").limit(5) }
 
 end
