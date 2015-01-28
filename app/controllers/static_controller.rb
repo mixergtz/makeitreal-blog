1dc07e349fe8b58e5eae279a4eae29ac6a74c627
Mixer Gutierrez <mixergutierrez@gmail.com>
2015-01-28 13:36:27 -0500

    Add static controller and update links

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def what_is
+  end
+
+  def team
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
 
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
-  get 'most_commented' => "most_commented#index"
 
+  get 'most_commented' => "most_commented#index"
   get "last_articles" => "last_articles#index"
 
   devise_for :users
+
   resources :articles do
     resources :comments
   end
 
-
+  get "what_is" => "static#what_is"
+  get "team" => "static#team"
+  get "contact" => "static#contac"
 
   root 'articles#index'
 
