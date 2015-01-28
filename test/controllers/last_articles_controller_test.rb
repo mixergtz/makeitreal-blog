c316d84d6b5dd49e216d3fb419f75e4b0d19be90
Mixer Gutierrez <mixergutierrez@gmail.com>
2015-01-28 17:24:26 -0500

    Clean unused files

--- test/controllers/last_articles_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LastArticlesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/helpers/last_articles_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class LastArticlesHelperTest < ActionView::TestCase
-end
