753ae49e158518789f65d6f8d0b2808c77e292e2
Mixer Gutierrez <mixergutierrez@gmail.com>
2015-01-29 12:31:32 -0500

    Fix home links

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
 
-  $("#index_articles").click(function(event) {
-    $(".home-links .home-link").removeClass("active");
-    $(this).parent().addClass("active");
-    $(".articles-preview").load("/ .articles-preview");
-  });
+  // $("#index_articles").click(function(event) {
+  //   $(".home-links .home-link").removeClass("active");
+  //   $(this).parent().addClass("active");
+  //   $(".articles-preview").load("/ .articles-preview");
+  // });
 
-  $("#last_articles").click(function(event) {
-    $(".home-links .home-link").removeClass("active");
-    $(this).parent().addClass("active");
-    $(".articles-preview").load("/last_articles .last-articles");
-  });
+  // $("#last_articles").click(function(event) {
+  //   $(".home-links .home-link").removeClass("active");
+  //   $(this).parent().addClass("active");
+  //   $(".articles-preview").load("/last_articles .last-articles");
+  // });
 
-  $("#most_commented").click(function(event) {
-    $(".home-links .home-link").removeClass("active");
-    $(this).parent().addClass("active");
-    $(".articles-preview").load("/most_commented .most-commented");
-  });
+  // $("#most_commented").click(function(event) {
+  //   $(".home-links .home-link").removeClass("active");
+  //   $(this).parent().addClass("active");
+  //   $(".articles-preview").load("/most_commented .most-commented");
+  // });
 
 
 });
